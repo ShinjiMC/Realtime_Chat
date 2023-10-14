056ce476586fc413a63bd4a56fb927f416603a5e
ShinjiMC <nayapbrau@gmail.com>
2023-10-14 02:37:00 -0500

    Connected backend with frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { connect, sendMsg } from "./api";
 
 function App() {
-  const [count, setCount] = useState(0)
+  connect();
+  const send = () => {
+    console.log("hello");
+    sendMsg("hello");
+  };
 
   return (
     <>
@@ -18,9 +22,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={send}>Hit</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/api/index.tsx
@@ -0,0 +1,24 @@
+const socket = new WebSocket("ws://localhost:3000/ws");
+
+const connect = () => {
+  console.log("Attempting Connection...");
+  socket.onopen = () => {
+    console.log("Successfully Connected");
+  };
+  socket.onmessage = (msg) => {
+    console.log(msg);
+  };
+  socket.onclose = (event) => {
+    console.log("Socket Closed Connection: ", event);
+  };
+  socket.onerror = (error) => {
+    console.log("Socket Error: ", error);
+  };
+};
+
+const sendMsg = (msg: string | ArrayBufferLike | Blob | ArrayBufferView) => {
+  console.log("sending msg: ", msg);
+  socket.send(msg);
+};
+
+export { connect, sendMsg };
